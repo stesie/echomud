f6f730bd80a604e378e56847e1420767178c5e99
Stefan Siegl <stesie@brokenpipe.de>
2017-01-25 09:00:50 +0100

    create room with Area instance

--- src/MudlibBridgeBundle/Command/MudlibCreateRoomCommand.php
+++ src/MudlibBridgeBundle/Command/MudlibCreateRoomCommand.php
@@ -3,10 +3,10 @@
 namespace MudlibBridgeBundle\Command;
 
 use stesie\mudlib\Command\CreateRoomCommand;
+use stesie\mudlib\ValueObject\Area;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class MudlibCreateRoomCommand extends ContainerAwareCommand
@@ -16,18 +16,22 @@ class MudlibCreateRoomCommand extends ContainerAwareCommand
         $this
             ->setName('mudlib:create-room')
             ->setDescription('Create a new room')
-            //->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
-            //->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('area', InputArgument::REQUIRED, 'Area, where the new room shall be located (like 3x3+0+0)')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        list($w, $h, $x, $y) = [null, null, null, null];
+
+        if (sscanf($input->getArgument('area'), '%ux%u%d%d', $w, $h, $x, $y) != 4) {
+            $output->writeln('Failed to parse area format');
+        }
+
         $command = new CreateRoomCommand($this->getContainer()->get('mudlib_bridge.event_store'));
-        $room = $command->run();
+        $room = $command->run(Area::createFromCoordinates($x, $y, $w, $h));
 
         $output->writeln('Room created, id=' . $room->getId());
-
         $this->getContainer()->get('doctrine.orm.entity_manager')->flush();
     }
 
